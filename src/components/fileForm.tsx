934a20a6a2af4ff6d6091b9b9aa9c3600f0441b9
Trent Conley <trentconley@gmail.com>
2023-08-10 21:37:37 -0500

    about edits

--- src/components/fileForm.tsx
+++ src/components/fileForm.tsx
@@ -1,25 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function FileUpload() {
-    const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
 
-
-    const handleFileSelected = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const files = event.target.files;
-        if (files && files.length > 0) {
-          const file = files[0];
-          setSelectedFile(file);
-        }
-      };
-
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        // Submit the form or do something else with the file data
-      }
-
-    if (selectedFile) {
-      // Do something with the selected file, such as upload it to a server
+  const handleFileSelected = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+    if (files && files.length > 0) {
+      const file = files[0];
+      setSelectedFile(file);
     }
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // Submit the form or do something else with the file data
+  };
+
+  if (selectedFile) {
+    // Do something with the selected file, such as upload it to a server
+  }
 
   return (
     <form onSubmit={handleSubmit}>
